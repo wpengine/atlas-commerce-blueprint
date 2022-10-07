ab8aaa4462f6790a69dd2eb69e74ed783aa867e0
Ross Maguire <ross.rmdesigns@gmail.com>
2022-10-07 14:22:50 +0100

    feat: add preview support for shop page and product post types

--- src/pages/preview.js
+++ src/pages/preview.js
@@ -2,10 +2,18 @@ import { client } from 'client';
 
 import { PostComponent } from './posts/[postSlug]';
 import { PageComponent } from './[...pageUri]';
+import { ProductComponent } from './product/[productSlug]';
+import { ShopComponent } from './shop';
 
 export default function Preview() {
   const isLoading = client.useIsLoading();
   const { typeName, node } = client.auth.usePreviewNode();
+
+  const products = client.useQuery().products({ first: 100 });
+  const product = client.useQuery().product({
+    id: node !== undefined ? node.productSlug : null,
+    idType: 'SLUG',
+  });
 
   if (isLoading || node === undefined) {
     return <p>Loading...</p>;
@@ -18,6 +26,12 @@ export default function Preview() {
   switch (typeName) {
     case 'Page': {
       const page = node;
+
+      if (page.slug === 'shop') {
+        // Will not work with an autosave version of the shop page as the slug changes
+        return <ShopComponent products={products?.nodes} />;
+      }
+
       return <PageComponent page={page} />;
     }
     case 'Post': {
@@ -25,6 +39,10 @@ export default function Preview() {
       return <PostComponent post={post} />;
     }
     // Add custom post types here as needed
+    case 'Product': {
+      return <ProductComponent product={product} />;
+    }
+
     default: {
       throw new Error(`Unknown post type: ${typeName}`);
     }
