383f6ecd9b9c7255d920ea4cf50d75e60e5a5f89
Ross Maguire <ross.rmdesigns@gmail.com>
2022-10-07 15:58:38 +0100

    refactor: move breadcrums to own component

--- src/components/index.js
+++ src/components/index.js
@@ -22,6 +22,7 @@ import SkipNavigationLink from './SkipNavigationLink';
 import TaxonomyTerms from './TaxonomyTerms';
 import ProductSummary from './ProductSummary';
 import ProductSort from './ProductSort';
+import BreadCrumbsMenu from './BreadCrumbsMenu';
 
 export {
   Button,
@@ -48,4 +49,5 @@ export {
   TaxonomyTerms,
   ProductSummary,
   ProductSort,
+  BreadCrumbsMenu,
 };
