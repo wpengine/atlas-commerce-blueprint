4ce2977fbd3af0ee855174a1f01349d02abece06
Ross Maguire <ross.rmdesigns@gmail.com>
2022-11-07 17:38:28 +0000

    chore: organising files into src dir

---
+++ src/components/ThemeStyles/ThemeStyles.js
@@ -0,0 +1,49 @@
+/* eslint-disable react/no-unknown-property */
+import appConfig from '../../../app.config';
+
+const themes = {
+  blue: {
+    '--color-black': '#000',
+    '--color-primary': '#000066',
+    '--color-secondary': '#0969da',
+    '--color-tertiary': '#CCCCCC',
+    '--color-white': '#FFFFFF',
+  },
+  red: {
+    '--color-black': '#000',
+    '--color-primary': '#660000',
+    '--color-secondary': '#B50505',
+    '--color-tertiary': '#CCCCCC',
+    '--color-white': '#FFFFFF',
+  },
+  green: {
+    '--color-black': '#000',
+    '--color-primary': '#006600',
+    '--color-secondary': '#c6c6c6',
+    '--color-tertiary': '#CCCCCC',
+    '--color-white': '#FFFFFF',
+  },
+  ecom: {
+    '--color-black': '#000',
+    '--color-primary': '#2a3533',
+    '--color-secondary': '#c6c6c6',
+    '--color-tertiary': '#CCCCCC',
+    '--color-white': '#FFFFFF',
+  },
+};
+
+export default function ThemeStyles() {
+  const themeColor = appConfig?.themeColor ?? 'blue';
+
+  return (
+    <style jsx global>{`
+      :root {
+        --color-black: ${themes[themeColor]['--color-black']};
+        --color-primary: ${themes[themeColor]['--color-primary']};
+        --color-secondary: ${themes[themeColor]['--color-secondary']};
+        --color-tertiary: ${themes[themeColor]['--color-tertiary']};
+        --color-white: ${themes[themeColor]['--color-white']};
+      }
+    `}</style>
+  );
+}
---
+++ src/pages/_app.js
@@ -0,0 +1,18 @@
+import '../../faust.config';
+import React from 'react';
+import { useRouter } from 'next/router';
+import { FaustProvider } from '@faustwp/core';
+import { AtlasEcomProvider } from '../hooks/useAtlasEcom';
+import '../styles/main.scss';
+
+export default function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  return (
+    <FaustProvider pageProps={pageProps}>
+      <AtlasEcomProvider>
+        <Component {...pageProps} key={router.asPath} />
+      </AtlasEcomProvider>
+    </FaustProvider>
+  );
+}
