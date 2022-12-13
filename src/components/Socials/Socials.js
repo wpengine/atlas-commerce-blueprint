98b5417d2dd6610e087e3f1c4dba486806dc8b71
Ross Maguire <ross.rmdesigns@gmail.com>
2022-12-13 16:17:12 +0000

    refactor: use map for rendering icons

--- src/components/Socials/Socials.js
+++ src/components/Socials/Socials.js
@@ -1,24 +1,18 @@
 import React from 'react';
 import styles from './Socials.module.scss';
+import icons from './_icons.js';
 
 const Socials = () => {
   return (
     <div className={styles.socials}>
-      <img
-        className={styles.socialIcon}
-        src='footer-icons/wordpress-icon.png'
-        alt='WordPress Icon'
-      />
-      <img
-        className={styles.socialIcon}
-        src='footer-icons/bigcommerce-icon.png'
-        alt='BigCommerce Icon'
-      />
-      <img
-        className={styles.socialIcon}
-        src='footer-icons/wp-engine-icon.png'
-        alt='BigCommerce Icon'
-      />
+      {icons.map((icon, idx) => (
+        <img
+          key={`social-icon-${idx}`}
+          className={styles.socialIcon}
+          alt={icon.alt}
+          src={icon.src}
+        />
+      ))}
     </div>
   );
 };
